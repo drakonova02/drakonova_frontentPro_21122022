6b4eff3ef35091a87eabae4ff4b932d4919ea36d
alesiad <drakonova02@gmail.com>
2022-12-21 01:05:00 +0100

    add check of time (>= 0)

--- HW5/index.js
+++ HW5/index.js
@@ -1,7 +1,7 @@
 let hours = Number(prompt('Input hours, please'));
 
-while(isNaN(hours)) {
-    hours = Number(prompt('Error: please input correct number of hours'));
+while(isNaN(hours) || hours < 0) {
+    hours = Number(prompt('Error: please input correct number of hours (number >= 0)'));
 }
 
 const seconds = (hours * 3600).toFixed(2);
